bf618868acda6fc361aa48627a821cf945e50185
Skullhack <vincent.givaudan@hotmail.fr>
2019-01-16 19:53:57 +0100

    Todolist fonctionnelle

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoServiceProvider } from '../todo/todo.service';
+import { TodoList } from '../todo/todo.model';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page implements OnInit {
+  todolists : TodoList[] = [];
+
+  constructor(private todoService : TodoServiceProvider) {
+    this.todoService.getList().subscribe((t : TodoList[]) => {
+      this.todolists = t;
+    })
+  }
+
+  ngOnInit() {
+    console.log("tab 2 initialisée");
+  }
+}
